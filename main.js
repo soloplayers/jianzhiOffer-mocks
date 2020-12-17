121fb519e5c021f5d706ea63c24d11d789c7f0b9
soloplayers <937059391@qq.com>
2020-12-17 14:44:24 +0800

    jianzhiOffer JZ3

--- main.js
+++ main.js
@@ -1,42 +1,28 @@
 // 个人题解
-/* 正则表达式 */
-function replaceSpace1(str)
+function ListNode(x){
+    this.val = x;
+    this.next = null;
+}
+function printListFromTailToHead(head)
 {
   // write code here
-  return str.replace(/ /g,"%20");
-}
-/* 另外解 */
-function replaceSpace2(str) {
-  let s='';
-  for(let i=0;i<str.length;i++){
-    if(str[i]!==' '){
-      s+=str[i];
-    }
-    else {
-      s+="%20";
-    }
+  let arr=[];
+  let node=head;
+  while(node){
+    arr.unshift(node.val);
+    node=node.next;
   }
-  return s;
+  return arr;
 }
+
 
 // 官方题解
 /*
-void replaceSpace(char *str,int length) {
-  if (str == nullptr || length <= 0) return; // 养成良好习惯，判空操作
-  int cnt = 0;  // 空格的个数
-  for (int i=0; i != length; ++i) {
-    if (str[i] == ' ') ++cnt;
-  }
-  if (!cnt) return; // 没有空格，直接返回
-  int new_length = length+cnt*2;
-  for (int i=length; i >= 0; --i) {
-    if (str[i] == ' ') {
-      str[new_length--] = '0';
-      str[new_length--] = '2';
-      str[new_length--] = '%';
-    }
-    else {
-      str[new_length--] = str[i];
-    }
-  }
+vector<int> printListFromTailToHead(ListNode* head) {
+  vector<int> ret;
+  if (!head)
+    return ret;
+  ret = printListFromTailToHead(head->next);
+  ret.push_back(head->val);
+  return ret;
 }*/
