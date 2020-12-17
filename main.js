9a271a8cab399ecf30925be565d3cae2ea68540a
soloplayers <937059391@qq.com>
2020-12-17 14:34:15 +0800

    jianzhiOffer JZ2

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
 // 个人题解
-function Find(target, array)
+/* 正则表达式 */
+function replaceSpace1(str)
 {
   // write code here
-  for(let i=0;i<array.length;i++){
-    for(let j=0;j<array.length;j++){
-      if(target===array[i][j]){
-        return true;
-      }
-    }
-  }
-  return false;
+  return str.replace(/ /g,"%20");
 }
-
-// 官方题解 -- 使用二分法
-function Find_std(target, array) {
-  // 判断数组是否为空
-  let m = array.length;
-  if (m === 0) return false;
-  let n = array[0].length;
-  if (n === 0) return false;
-  let r = 0, c = n-1; // 右上角元素
-  while (r<m && c>=0) {
-    if (target === array[r][c]) {
-      return true;
-    }
-    else if (target > array[r][c]) {
-      ++r;
+/* 另外解 */
+function replaceSpace2(str) {
+  let s='';
+  for(let i=0;i<str.length;i++){
+    if(str[i]!==' '){
+      s+=str[i];
     }
     else {
-      --c;
+      s+="%20";
     }
   }
-  return false;
+  return s;
 }
+
+// 官方题解
+/*
+void replaceSpace(char *str,int length) {
+  if (str == nullptr || length <= 0) return; // 养成良好习惯，判空操作
+  int cnt = 0;  // 空格的个数
+  for (int i=0; i != length; ++i) {
+    if (str[i] == ' ') ++cnt;
+  }
+  if (!cnt) return; // 没有空格，直接返回
+  int new_length = length+cnt*2;
+  for (int i=length; i >= 0; --i) {
+    if (str[i] == ' ') {
+      str[new_length--] = '0';
+      str[new_length--] = '2';
+      str[new_length--] = '%';
+    }
+    else {
+      str[new_length--] = str[i];
+    }
+  }
+}*/
